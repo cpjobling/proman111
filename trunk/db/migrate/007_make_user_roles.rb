f5cea1cb9a2545a29474701d7b94bfa53f53920e
cpjobling <cpjobling@961f3832-2ed1-11de-bfb9-e98281094dd5>
2009-05-07 16:19:39 +0000

    Added some extra validation rules. Slight mod to 007_make_user_roles.rb.

--- trunk/db/migrate/007_make_user_roles.rb
+++ trunk/db/migrate/007_make_user_roles.rb
@@ -6,13 +6,14 @@ class MakeUserRoles < ActiveRecord::Migration
     admin.save
 
     student_role = Role.find_by_sql("select id from roles where name='student';")
-    student = User.find(2); # student user
+    student = User.find(2); # dummy student user
     student.roles=student_role
     student.save
 
     staff_role = Role.find_by_sql("select id from roles where name='staff';")
-    staff = User.find(3); # staff user
+    staff = User.find(3); # Real staff user (Dr C.P. Jobling)
     staff.roles=staff_role
+    staff.roles=admin_role
     staff.save
   end
 
