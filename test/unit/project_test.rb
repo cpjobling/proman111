5a2eb4b869a2d46ac575d8a39fa699c2a5ad62a1
cpjobling <cpjobling@961f3832-2ed1-11de-bfb9-e98281094dd5>
2009-05-12 21:54:32 +0000

    Added tests for project.

--- test/unit/project_test.rb
+++ test/unit/project_test.rb
@@ -8,4 +8,36 @@ class ProjectTest < ActiveSupport::TestCase
   def test_truth
     assert true
   end
+  
+  # Test validation rules
+  def test_invalid_with_empty_atributes
+  	project = Project.new
+  	assert !project.valid?
+  	assert project.errors.invalid?(:title)
+  	assert project.errors.invalid?(:description)
+  	assert project.errors.invalid?(:created_by)
+  end
+  
+  def test_unique_title
+  	project = Project.new(:title => projects(:project1).title,
+  		:description => "A dummy project",
+  		:created_by => users(:academic).id)
+  		
+  	assert !project.save
+  	assert_equal "has already been taken", project.errors.on(:title)
+  end
+  
+  def test_project_creator_is_valid
+      users = User.find(:all)
+      users.each do |user|
+      	if user.has_role?('staff')
+			projects = Project.find(:all, 
+			   :conditions => ["created_by = ?", user.id])
+			projects.each do |project|
+				assert_equal project.created_by, user.id, 
+				   "user didn't create project" 
+			end
+      	end	
+  	  end
+  end
 end
