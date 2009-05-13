27537d74613e6ea3e4f5d7349a8173d0019d08b1
cpjobling <cpjobling@961f3832-2ed1-11de-bfb9-e98281094dd5>
2009-05-13 19:22:39 +0000

    Added utility methods suitable_for, suitable_for_all, suitable_for_any, suitable_for_none and various queries to manage disciplines for the project model class. All new methods supported by unit tests.
    Updated disciplines file to match reality.

--- trunk/test/unit/project_test.rb
+++ trunk/test/unit/project_test.rb
@@ -4,6 +4,12 @@ class ProjectTest < ActiveSupport::TestCase
 	
   fixtures :users, :projects
   
+  def setup
+    @project = projects(:project1)
+    @disciplines = Discipline.find(:all)
+    @icct = disciplines(:icct)	
+  end
+  
   # Replace this with your real tests.
   def test_truth
     assert true
@@ -42,11 +48,103 @@ class ProjectTest < ActiveSupport::TestCase
   end
   
   def test_project_can_be_carbon_critical
-  	project = projects(:project1)
-  	assert !project.carbon_critical, 
-  		"project by defualt should not be carbon critical"
-  	project.carbon_critical = true
-  	assert project.carbon_critical, 
+  	assert !@project.carbon_critical, 
+  		"project by default should not be carbon critical"
+  	@project.carbon_critical = true
+  	assert @project.carbon_critical, 
   		"project by should now be carbon critical"
+  end
+  
+  def test_project_not_assigned_to_discipline
+  	@disciplines.each do |discipline|
+  		assert !@project.suitable_for?(discipline.name)
+  	end
+  end
+  
+  def test_project_assigned_to_discipline
+  	discipline = @icct
+  	@project.disciplines << discipline
+  	assert @project.suitable_for?(discipline.name)
+  end
+  
+  def test_project_suitable_for_all
+  	@disciplines.each do |discipline|
+  		@project.disciplines << discipline
+  	end
+    assert @project.suitable_for_all?, 
+       "Project should suit all disciplines"
+  end
+  
+  def test_unnassigned_project_not_suitable_for_all
+    assert ! @project.suitable_for_all?, 
+       "Unaasigned project should not suit all disciplines"
+  end
+  
+  def test_singly_assigned_project_not_suitable_for_all
+  	@project.disciplines << @icct
+    assert ! @project.suitable_for_all?, 
+       "Project assigned to one discipline should not suit all disciplines"
+  end
+  
+  def test_project_assigned_to_all_but_one_discipline_not_suitable_for_all
+  	@disciplines.each do |discipline|
+  		@project.disciplines << discipline
+  	end
+  	
+  	# Remove last discipline
+  	@project.disciplines.delete(disciplines(:sport))
+    assert ! @project.suitable_for_all?, 
+       "Project assigned to all but one discipline should not suit all disciplines"
+  end
+  
+  def test_suitable_for_any
+  	assert ! @project.suitable_for_any?, "Project should not be suitable for any"
+  	@project.disciplines << @icct
+  	assert @project.suitable_for_any?, "Project should be suitable for any"
+  end
+
+  def test_suitable_for_discipline
+  	assert ! @project.suitable_for_any?, 
+  	    "Project should not be suitable for any discipine"
+  	discipline = @icct
+  	@project.suitable_for(discipline.name)
+  	assert @project.suitable_for?(discipline.name),
+  	    "project should now be suitable for #{discipline.name}"
+  end
+  
+  def test_cant_add_unknown_discipline_to_project
+  	@project.suitable_for('cheesemakers')
+  	assert @project.disciplines.count(:all) == 0,
+  	  "There should be no cheesemakers"
+  end
+  
+  def test_cant_add_duplicate_discipline_to_project
+  	@project.suitable_for(@icct.name)
+  	assert @project.disciplines.count(:all) == 1,
+  	  "Should be 1 suitable discipline for project"
+  	@project.suitable_for(@icct.name)
+  	assert @project.disciplines.count(:all) == 1,
+  	  "Should still be 1 suitable discipline for project"
+  	@project.suitable_for(disciplines(:eee).name)
+  	assert @project.disciplines.count(:all) == 2,
+  	  "Should now be 2 suitable discipline for project"
+  end
+  
+  def test_suitable_for_all
+  	@project.suitable_for_all
+  	assert @project.suitable_for_all?, "Project is suitable for all"
+  end
+  
+  def test_discipline_suitable_for_none
+  	@project.suitable_for_all
+  	@project.suitable_for_none
+  	assert ! @project.suitable_for_all?, "Project is suitable for all"
+  	assert ! @project.suitable_for_any?, "Project is not suitable for any"
+  end
+  
+  def test_discipline_suitable_for_none?
+  	@project.suitable_for_all
+  	@project.suitable_for_none
+  	assert @project.suitable_for_none?, "Project is suitable for none"
   end
 end
